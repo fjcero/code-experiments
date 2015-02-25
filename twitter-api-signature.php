1d4535fcf8d1caea9869965eb88277175fed55f8
Francisco J. Caballero <fjcero@gmail.com>
2015-02-25 12:25:13 -0300

    Indent using spaces

--- twitter-api-signature.php
+++ twitter-api-signature.php
@@ -3,7 +3,7 @@
 $method   = "get";
 $endpoint = "https://api.twitter.com/1.1/users/search.json";
 $query    = [
-	'q' => 'fjcero',
+    'q' => 'fjcero',
 ];
 
 // Required Keys
@@ -17,12 +17,12 @@ $oauth_token_secret = "the-oauth-token-secret";
 // The easy way: https://dev.twitter.com/oauth/tools/signature-generator/7964289
 
 $body = [
-	'oauth_consumer_key'     => $consumer_key,
-	'oauth_nonce'            => md5(microtime() . mt_rand()),
-	'oauth_signature_method' => 'HMAC-SHA1',
-	'oauth_timestamp'        => time(),
-	'oauth_version'          => '1.0',
-	'oauth_token'            => $oauth_token,
+    'oauth_consumer_key'     => $consumer_key,
+    'oauth_nonce'            => md5(microtime() . mt_rand()),
+    'oauth_signature_method' => 'HMAC-SHA1',
+    'oauth_timestamp'        => time(),
+    'oauth_version'          => '1.0',
+    'oauth_token'            => $oauth_token,
 ];
 
 $params = array_unique(array_merge($query, $body));
@@ -30,12 +30,12 @@ ksort($params);
 $params = http_build_query($params, null, '&', PHP_QUERY_RFC3986);
 
 $signature = base64_encode( 
-	hash_hmac(
-		'SHA1', 
-		strtoupper($method).'&'.urlencode($endpoint).'&'.urlencode($params),
-		urlencode($consumer_secret).'&'.urlencode($oauth_token_secret),
-		true
-	)
+    hash_hmac(
+        'SHA1', 
+        strtoupper($method).'&'.urlencode($endpoint).'&'.urlencode($params),
+        urlencode($consumer_secret).'&'.urlencode($oauth_token_secret),
+        true
+    )
 );
 
 $body['oauth_signature'] = $signature;
@@ -43,8 +43,8 @@ $authorization = http_build_query($body, null, ', ');
 
 // This output could be used in CLI
 echo vsprintf('curl --%s \'%s\' --data \'%s\' --header \'Authorization: OAuth %s\' --verbose', [ 
-	$method,
-	$endpoint,
-	http_build_query($query, null, '&'),
-	$authorization
+    $method,
+    $endpoint,
+    http_build_query($query, null, '&'),
+    $authorization
 ]);
